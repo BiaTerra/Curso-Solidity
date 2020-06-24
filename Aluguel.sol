e5514a8e8c8a34209ffc64de83824dd166a72bad
BiaTerra <67346999+BiaTerra@users.noreply.github.com>
2020-06-23 22:21:01 -0300

    Create Aluguel.sol
    Versão inicial do contrato.

---
+++ Aluguel.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GLP-3.0
+pragma solidity 0.6.10;
+
+contract Aluguel {
+    
+    string public locador;
+    string public locatario;
+    uint256 private valor;
+    uint256 constant numeroMaximoLegalDeAlugueisParaMulta = 3;
+    
+    constructor (
+        string memory nomeLocador,
+        string memory nomeLocatario,
+        uint256 valorDoAluguel
+        )
+    public
+{
+        locador = nomeLocador;
+        locatario = nomeLocatario;
+        valor = valorDoAluguel;
+}
+
+}
