0ea99b396926cf3971db10c2a2bf0ee2c880406d
BiaTerra <67346999+BiaTerra@users.noreply.github.com>
2020-06-24 22:44:40 -0300

    Update Aluguel.sol
    Inclusão da função de simulação de multa e reajuste de aluguel.

--- Aluguel.sol
+++ Aluguel.sol
@@ -14,15 +14,34 @@ contract Aluguel {
         uint256 valorDoAluguel
         )
     public
-{
+    {
         locador = nomeLocador;
         locatario = nomeLocatario;
         valor = valorDoAluguel;
-}
+    }
 
     function valorAtualDoAluguel () public view returns (uint256) {
         return valor;
         
     }
 
+    function simulaMulta (uint256 mesesRestantes, uint256 totalMesesContrato) public view returns (uint256 valorMulta) {
+        
+        valorMulta = valor*numeroMaximoLegalDeAlugueisParaMulta;
+        valorMulta = valorMulta/totalMesesContrato;
+        valorMulta = valorMulta*mesesRestantes;
+        return valorMulta;
+    
+    }
+
+    function reajustaAluguel (uint256 percentualReajuste) public {
+        if (percentualReajuste > 20) {
+            percentualReajuste = 20;
+        }
+        uint256 valorDoAcrescimo;
+        valorDoAcrescimo = ((valor*percentualReajuste)/100);
+        valor = valor + valorDoAcrescimo;
+    }
+        
 }
+  
