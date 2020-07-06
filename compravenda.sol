489924511aad75e05350db3bf2f4b544914f3f1b
BiaTerra <67346999+BiaTerra@users.noreply.github.com>
2020-07-06 20:44:21 -0300

    Update compravenda.sol
    Inclusão de cláusulas relativas ao direito de preferência dos locatários.

--- compravenda.sol
+++ compravenda.sol
@@ -3,11 +3,25 @@ pragma solidity 0.6.10;
 
 contract CompraEVenda {
     
+    struct Locatario {
+        string nomeLocatario;
+        address enderecoCarteira;
+        uint256 valor;
+        bool exercicioPreferencia;
+    }
+    
     string public comprador;
     string public vendedor;
+    address payable public contaVendedor;
     uint256 public preco;
     uint256 public constant parcelas = 10;
     uint256 public valorParcela;
+    uint256 public prazoPreferencia = now + 2592000;
+    
+    mapping (nomeLocatario => Locatario) public listaLocatarios;
+    Locatario[] public locatarios;
+    
+    event exercicioDireitoPreferencia (nomeLocatario);
     
     constructor (
         string memory nomeComprador, 
